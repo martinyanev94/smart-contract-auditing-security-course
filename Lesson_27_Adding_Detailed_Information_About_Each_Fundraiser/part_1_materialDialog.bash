cefbe9f7aea34d754f447464c8514e78dc85fce6
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-08 00:02:56 -0400

    Add Lesson_27_Adding_Detailed_Information_About_Each_Fundraiser/part_1_materialDialog.bash

---
+++ Lesson_27_Adding_Detailed_Information_About_Each_Fundraiser/part_1_materialDialog.bash
@@ -0,0 +1,18 @@
+npm install @material-ui/core
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  card: {
+    maxWidth: 450,
+    height: 400,
+  },
+}));
