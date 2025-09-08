93757c2c0ea3c555fffe28fda2cde216da4a8342
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-08 00:02:13 -0400

    Add Lesson_22_Web3_Methods_And_Interaction/part_4_sendTransactionExample.js

---
+++ Lesson_22_Web3_Methods_And_Interaction/part_4_sendTransactionExample.js
@@ -0,0 +1,9 @@
+web3.eth.sendTransaction({
+    from: '0xa589BDa5379fDE626dD3eBF462fA22F5dfF43f30', // Sender’s address
+    to: '0xa289BDa5379fDE626dM3eBF462fA22F5dfF43f31', // Receiver’s address
+    value: web3.utils.toWei('0.1', 'ether')  // Sending 0.1 Ether
+}).then(transactionReceipt => {
+    console.log(`Transaction successful with hash: ${transactionReceipt.transactionHash}`);
+}).catch(error => {
+    console.error("Transaction failed:", error.message);
+});
