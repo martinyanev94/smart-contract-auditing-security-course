633df05b887b7773f9e8d384bc739a0bd4df40aa
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-08 00:03:09 -0400

    Add Lesson_28_Accepting_A_Donation_With_React/part_2_donationDialog.js

---
+++ Lesson_28_Accepting_A_Donation_With_React/part_2_donationDialog.js
@@ -0,0 +1,8 @@
+<DialogActions>
+  <Button onClick={submitFunds} variant="contained" color="primary">
+    Donate
+  </Button>
+  <Button onClick={handleClose} color="secondary">
+    Cancel
+  </Button>
+</DialogActions>
