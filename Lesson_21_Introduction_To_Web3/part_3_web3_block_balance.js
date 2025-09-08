ff16a7e3d575174facbfb3d9a6a5aaa97bce3d80
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-08 00:02:03 -0400

    Add Lesson_21_Introduction_To_Web3/part_3_web3_block_balance.js

---
+++ Lesson_21_Introduction_To_Web3/part_3_web3_block_balance.js
@@ -0,0 +1,9 @@
+web3.eth.getBlockNumber().then((blockNumber) => {
+    console.log(`Current block number: ${blockNumber}`);
+});
+const accountAddress = '0xa589BDa5379fDE626dD3eBF462fA22F5dfF43f30'; // Replace with your address
+
+web3.eth.getBalance(accountAddress)
+    .then(balance => {
+        console.log(`Balance for address ${accountAddress}: ${web3.utils.fromWei(balance, 'ether')} ETH`);
+    });
